e4fe8ff4e46ceb0b1023eb4cd20d4128da0caca4
hoffmannMuki <47600141+hoffmannMuki@users.noreply.github.com>
2019-11-11 14:30:48 +0000

    Update DateRange.java

--- src/uk/ac/ed/bikerental/DateRange.java
+++ src/uk/ac/ed/bikerental/DateRange.java
@@ -1,58 +1,64 @@
-package uk.ac.ed.bikerental;
-
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.Objects;
 import java.util.function.BooleanSupplier;
 
 public class DateRange {
-    private LocalDate start, end;
-    
-    public DateRange(LocalDate start, LocalDate end) {
-        this.start = start;
-        this.end = end;
-    }
-    
-    public LocalDate getStart() {
-        return this.start;
-    }
-    
-    public LocalDate getEnd() {
-        return this.end;
-    }
+	private LocalDate start, end;
 
-    public long toYears() {
-        return ChronoUnit.YEARS.between(this.getStart(), this.getEnd());
-    }
+	public DateRange(LocalDate start, LocalDate end) {
+		this.start = start;
+		this.end = end;
+	}
 
-    public long toDays() {
-        return ChronoUnit.DAYS.between(this.getStart(), this.getEnd());
-    }
+	public LocalDate getStart() {
+		return this.start;
+	}
 
-    public Boolean overlaps(DateRange other) {
-        // TODO: implement date range intersection checking
-        assert false;
-        return null;
-    }
+	public LocalDate getEnd() {
+		return this.end;
+	}
+	
+	public long toYears() {
+		return ChronoUnit.YEARS.between(this.getStart(), this.getEnd());
+	}
+	
+	public long toDays() {
+		return ChronoUnit.DAYS.between(this.getStart(), this.getEnd());
+	}
 
-    @Override
-    public int hashCode() {
-        // hashCode method allowing use in collections
-        return Objects.hash(end, start);
-    }
+	public Boolean overlaps(DateRange other) {
+		if (other == null)
+			return false;
+		if (this.getStart().isAfter(other.getStart())
+		||  this.getStart().isEqual(other.getStart())) {
+			if (this.getEnd().isAfter(other.getEnd()))
+				return true;
+			else
+				return false;
+		}
+		else
+			return true;
+	}
+	
+	@Override
+	public int hashCode() {
+		// hashCode method allowing use in collections
+		return Objects.hash(end, start);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		// equals method for testing equality in tests
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DateRange other = (DateRange) obj;
+		return Objects.equals(end, other.end) && Objects.equals(start, other.start);
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        // equals method for testing equality in tests
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        DateRange other = (DateRange) obj;
-        return Objects.equals(end, other.end) && Objects.equals(start, other.start);
-    }
-    
-    // You can add your own methods here
+	// You can add your own methods here
 }
