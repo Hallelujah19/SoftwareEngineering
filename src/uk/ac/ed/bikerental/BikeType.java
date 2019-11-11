ba2b608d0660a3278d9f293192168091b122862a
Hallelujah19 <43917528+Hallelujah19@users.noreply.github.com>
2019-11-11 14:23:17 +0000

    Update BikeType.java

--- src/uk/ac/ed/bikerental/BikeType.java
+++ src/uk/ac/ed/bikerental/BikeType.java
@@ -3,10 +3,56 @@ package uk.ac.ed.bikerental;
 import java.math.BigDecimal;
 import java.util.Objects;
 
-public class BikeType {
-    public BigDecimal getReplacementValue() {
-        // TODO: Implement Bike.getReplacementValue
-        assert false;
-        return null;
-    }
-}
+	
+	public class BikeType {
+		
+		protected String type;
+		private BigDecimal replacementValue;//Original price assumed to equal this
+		private BigDecimal dailyPrice;
+		private BigDecimal depositRate;
+		
+		
+		
+		public String getType() {
+			return this.type;
+		}
+
+
+		public void setType(String type) {
+			this.type = type;
+		}
+
+
+		public BigDecimal getReplacementValue() {
+	        // TODO: Implement Bike.getReplacementValue
+	       return this.replacementValue;
+	    }
+
+
+		public void setReplacementValue(BigDecimal replacementValue) {
+			this.replacementValue = replacementValue;
+		}
+
+
+		public void setDailyPrice(BigDecimal dailyPrice) {
+			this.dailyPrice = dailyPrice;
+		}
+
+
+		public void setDepositRate(BigDecimal depositRate) {
+			this.depositRate = depositRate;
+		}
+
+
+		public BigDecimal getDailyPrice() {
+			return dailyPrice;
+		}
+
+
+		public BigDecimal getDepositRate() {
+			return depositRate;
+		}
+		
+		
+		
+	}
