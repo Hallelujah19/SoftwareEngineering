e83884b17ebb0e70c7a142aa0158899a8683381d
Hallelujah19 <43917528+Hallelujah19@users.noreply.github.com>
2019-11-11 16:01:30 +0000

    Update Bike.java

--- src/uk/ac/ed/bikerental/Bike.java
+++ src/uk/ac/ed/bikerental/Bike.java
@@ -1,9 +1,104 @@
-package uk.ac.ed.bikerental;
+
+import java.time.LocalDate;
 
 public class Bike {
-    public BikeType getType() {
-        // TODO: Implement Bike.getType
-        assert false;
-        return null;
-    }
+	public static enum DepositStatus
+	{
+		RECIEVED,NA
+
+		}
+
+	public static enum BikeStatus{
+		
+		HIRED,AVAILABLE,RESERVED
+		
+	}
+	  
+	 
+	  private BikeStatus bikeStatus;
+	  private BikeType bikeType;
+	  private Double originalValue;
+	  private String bikeId;
+	  private LocalDate availableFromDate;
+	  private DepositStatus depositstatus;
+	  
+		  
+	    public BikeType getType() {
+	        // TODO: Implement Bike.getType
+	    	
+	    	return this.bikeType;
+	       }
+
+
+	
+        
+		public BikeStatus getBikeStatus() {
+			return bikeStatus;
+		}
+
+
+		public BikeType getBikeType() {
+			return this.bikeType;
+		}
+
+
+		public Double getOriginalValue() {
+			return originalValue;
+		}
+
+
+		public String getBikeId() {
+			return bikeId;
+		}
+
+
+		public LocalDate getAvailableFromDate() {
+			return availableFromDate;
+		}
+
+
+		
+
+
+		public void setBikeStatus(BikeStatus bikeStatus) {
+			this.bikeStatus = bikeStatus;
+		}
+
+
+		public void setBikeType(BikeType bikeType) {
+			this.bikeType = bikeType;
+		}
+
+
+		public void setOriginalValue(Double originalValue) {
+			this.originalValue = originalValue;
+		}
+
+
+		public void setBikeId(String bikeId) {
+			this.bikeId = bikeId;
+		}
+
+
+		public void setAvailableFromDate(LocalDate availableFromDate) {
+			this.availableFromDate = availableFromDate;
+		}
+
+
+		public void setDepositstatus(DepositStatus depositstatus) {
+			this.depositstatus = depositstatus;
+		}
+
+
+		public DepositStatus getDepositstatus() {
+			return depositstatus;
+		}
+	    
+	    
+	    
+	    
+	    
+	    
+	
+
 }
