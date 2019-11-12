01c517538046c2e77b94bd9e8719162111db25ac
hoffmannMuki <47600141+hoffmannMuki@users.noreply.github.com>
2019-11-12 00:18:01 +0000

    Update Bike.java
    Converted double types to BigDecimal for better precision.
    Added dateRange attribute to help determine the availability of a bike
    Removed availableFromDate attribute

--- src/uk/ac/ed/bikerental/Bike.java
+++ src/uk/ac/ed/bikerental/Bike.java
@@ -1,104 +1,78 @@
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public class Bike {
-	public static enum DepositStatus
-	{
-		RECIEVED,NA
+	public static enum DepositStatus {
+		RECIEVED, NA
 
-		}
-
-	public static enum BikeStatus{
-		
-		HIRED,AVAILABLE,RESERVED
-		
 	}
-	  
-	 
-	  private BikeStatus bikeStatus;
-	  private BikeType bikeType;
-	  private Double originalValue;
-	  private String bikeId;
-	  private LocalDate availableFromDate;
-	  private DepositStatus depositstatus;
-	  
-		  
-	    public BikeType getType() {
-	        // TODO: Implement Bike.getType
-	    	
-	    	return this.bikeType;
-	       }
 
+	public static enum BikeStatus {
 
-	
-        
-		public BikeStatus getBikeStatus() {
-			return bikeStatus;
-		}
+		HIRED, AVAILABLE, RESERVED
 
+	}
 
-		public BikeType getBikeType() {
-			return this.bikeType;
-		}
+	private BikeStatus bikeStatus;
+	private BikeType bikeType;
+	private BigDecimal originalValue;
+	private String bikeId;
+	private DateRange dateRange;
+	private DepositStatus depositStatus;
 
+	public BikeType getType() {
+		// TODO: Implement Bike.getType
 
-		public Double getOriginalValue() {
-			return originalValue;
-		}
+		return this.bikeType;
+	}
 
+	public BikeStatus getBikeStatus() {
+		return bikeStatus;
+	}
 
-		public String getBikeId() {
-			return bikeId;
-		}
+	public BikeType getBikeType() {
+		return this.bikeType;
+	}
 
+	public BigDecimal getOriginalValue() {
+		return originalValue;
+	}
 
-		public LocalDate getAvailableFromDate() {
-			return availableFromDate;
-		}
+	public String getBikeId() {
+		return bikeId;
+	}
 
+	public DateRange getDateRange() {
+		return dateRange;
+	}
 
-		
+	public void setBikeStatus(BikeStatus bikeStatus) {
+		this.bikeStatus = bikeStatus;
+	}
 
+	public void setBikeType(BikeType bikeType) {
+		this.bikeType = bikeType;
+	}
 
-		public void setBikeStatus(BikeStatus bikeStatus) {
-			this.bikeStatus = bikeStatus;
-		}
+	public void setOriginalValue(BigDecimal originalValue) {
+		this.originalValue = originalValue;
+	}
 
+	public void setBikeId(String bikeId) {
+		this.bikeId = bikeId;
+	}
 
-		public void setBikeType(BikeType bikeType) {
-			this.bikeType = bikeType;
-		}
+	public void setDateRange(DateRange dateRange) {
+		this.dateRange = dateRange;
+	}
 
+	public void setDepositstatus(DepositStatus depositstatus) {
+		this.depositStatus = depositstatus;
+	}
 
-		public void setOriginalValue(Double originalValue) {
-			this.originalValue = originalValue;
-		}
-
-
-		public void setBikeId(String bikeId) {
-			this.bikeId = bikeId;
-		}
-
-
-		public void setAvailableFromDate(LocalDate availableFromDate) {
-			this.availableFromDate = availableFromDate;
-		}
-
-
-		public void setDepositstatus(DepositStatus depositstatus) {
-			this.depositstatus = depositstatus;
-		}
-
-
-		public DepositStatus getDepositstatus() {
-			return depositstatus;
-		}
-	    
-	    
-	    
-	    
-	    
-	    
-	
+	public DepositStatus getDepositstatus() {
+		return depositStatus;
+	}
 
 }
