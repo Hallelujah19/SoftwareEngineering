fb2e539420cb78b08c30a309a197e22a10059b85
hoffmannMuki <47600141+hoffmannMuki@users.noreply.github.com>
2019-11-12 13:36:11 +0000

    Create Quote.java

---
+++ src/Quote.java
@@ -0,0 +1,11 @@
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Quote {
+	
+	private BikeProvider bikeProvider;
+	private BigDecimal totalPrice;
+	private BigDecimal deposit;
+	private LocalDate dateOfReturn;
+
+}
