da2c88d0420da38170237faca9b0688e6f3b5ebb
hoffmannMuki <47600141+hoffmannMuki@users.noreply.github.com>
2019-11-15 22:34:12 +0000

    Update BikeProvider.java

--- src/uk/ac/ed/bikerental/BikeProvider.java
+++ src/uk/ac/ed/bikerental/BikeProvider.java
@@ -3,9 +3,9 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 
 public class BikeProvider {
-	
+
 	private String name;
-	private HashMap<BikeType,Integer> stockOfBikes; // for the bike provider
+	private HashMap<BikeType, Integer> stockOfBikes; // for the bike provider
 	private ArrayList<Bike> bikes;
 	private Location shopLocation;
 	private OpeningHours openingHours;
@@ -13,78 +13,100 @@ public class BikeProvider {
 	private String messageFromPartner;
 	private String messageToPartner;
 	private ArrayList<String> bookingNumbers; // store unique booking numbers
-	private HashMap<BikeType,Integer> numOfBikesPerType;
 	private HashMap<BikeType, BigDecimal> dailyPricePerBikeType;
 	private BigDecimal depositRate;
-	
-	public BikeProvider (String name, Location shopLocation,OpeningHours openingHours) {
+
+	public BikeProvider(String name, Location shopLocation, OpeningHours openingHours) {
 		this.name = name;
 		this.shopLocation = shopLocation;
 		this.openingHours = openingHours;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
+
 	public HashMap<BikeType, Integer> getStockOfBikes() {
 		return stockOfBikes;
 	}
+
 	public ArrayList<Bike> getBikes() {
 		return bikes;
 	}
+
 	public Location getShopLocation() {
 		return shopLocation;
 	}
+
 	public OpeningHours getOpeningHours() {
 		return openingHours;
 	}
+
 	public BikeProvider getPartnerProvider() {
 		return partnerProvider;
 	}
+
 	public String getMessageFromPartner() {
 		return messageFromPartner;
 	}
+
 	public String getMessageToPartner() {
 		return messageToPartner;
 	}
-	
-	public HashMap<BikeType, Integer> getNumOfBikesPerType() {
-		return numOfBikesPerType;
-	}
+
 	public HashMap<BikeType, BigDecimal> getDailyPricePerBikeType() {
 		return dailyPricePerBikeType;
 	}
+
 	public void updateName(String name) {
 		this.name = name;
 	}
+
 	public void setStockOfBikes(HashMap<BikeType, Integer> stockOfBikes) {
 		this.stockOfBikes = stockOfBikes;
 	}
-	public void setBikes(ArrayList<Bike> bikes) {
-		this.bikes = bikes;
+	
+	public void setBike(Bike bike) {
+		this.bikes.add(bike);
 	}
+	
 	public void updateShopLocation(Location shopLocation) {
 		this.shopLocation = shopLocation;
 	}
+
 	public void updateOpeningHours(OpeningHours openingHours) {
 		this.openingHours = openingHours;
 	}
+
+	public void updateBikeStatus(String bikeId, BikeStatus bikeStatus) {
+		for (int i = 0; i < bikes.size(); i++) {
+			if (bikes.get(i).getBikeId().equals(bikeId)) {
+				bikes.get(i).setBikeStatus(bikeStatus);
+				break;
+			}
+		}
+	}
+	
 	public BigDecimal getDepositRate() {
 		return depositRate;
 	}
+	
 	public void setDepositRate(BigDecimal depositRate) {
 		this.depositRate = depositRate;
 	}
+
 	public void setPartnerProvider(BikeProvider partnerProvider) {
 		this.partnerProvider = partnerProvider;
 	}
+
 	public void setMessageFromPartner(String messageFromPartner) {
 		this.messageFromPartner = messageFromPartner;
 	}
+
 	public void setMessageToPartner(String messageToPartner) {
 		this.messageToPartner = messageToPartner;
 	}
-	
+
 	public ArrayList<String> getBookingNumbers() {
 		return bookingNumbers;
 	}
@@ -93,11 +115,8 @@ public class BikeProvider {
 		this.bookingNumbers = bookingNumbers;
 	}
 
-	public void setNumOfBikesPerType(HashMap<BikeType, Integer> numOfBikesPerType) {
-		this.numOfBikesPerType = numOfBikesPerType;
-	}
 	public void setDailyPricePerBikeType(HashMap<BikeType, BigDecimal> dailyPricePerBikeType) {
 		this.dailyPricePerBikeType = dailyPricePerBikeType;
 	}
-	
+
 }
