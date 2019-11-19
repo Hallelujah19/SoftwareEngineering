367b2d445b34a70c96553740769f71e172e7c3e3
Hallelujah19 <43917528+Hallelujah19@users.noreply.github.com>
2019-11-19 17:00:10 +0000

    Update ImplementPricingPolicyTest.java

--- src/uk/ac/ed/bikerental/ImplementPricingPolicyTest.java
+++ src/uk/ac/ed/bikerental/ImplementPricingPolicyTest.java
@@ -9,8 +9,19 @@ BigDecimal Test = BigDecimal("100");
 AssertEquals(Test,subject.SetPrice(100));
 
 @Test
+ 
+ Collection<Bikes> testBikes = new ArrayList();
+ 
+  testBikes.add();
+  .
+  .
+  .
+  .
+  .
+  
+ BigDecimal TestPrice = BigDecimal("x");
 
-
+ AsserEquals(TestPrice,calculatePrice(Collection<Bike> bikes, DateRange duration));
 
 
 
