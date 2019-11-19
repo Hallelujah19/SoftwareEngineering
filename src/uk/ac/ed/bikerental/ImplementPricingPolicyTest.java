b482281d7322aa7d697a568ab318015d13f8b964
Hallelujah19 <43917528+Hallelujah19@users.noreply.github.com>
2019-11-19 16:53:41 +0000

    Create ImplementPricingPolicyTest.java

---
+++ src/uk/ac/ed/bikerental/ImplementPricingPolicyTest.java
@@ -0,0 +1,30 @@
+class ImplementPricingPolicy{
+
+ImplementPrcingPolicy subject=new ImplementPricingPolicy();
+
+@Test
+
+BigDecimal Test = BigDecimal("100");
+
+AssertEquals(Test,subject.SetPrice(100));
+
+@Test
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
