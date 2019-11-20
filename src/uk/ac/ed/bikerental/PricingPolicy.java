3e01d7212450ea1316d56a67f7b8871ae6333fb6
hoffmannMuki <47600141+hoffmannMuki@users.noreply.github.com>
2019-11-20 22:10:48 +0000

    Update PricingPolicy.java

--- src/uk/ac/ed/bikerental/PricingPolicy.java
+++ src/uk/ac/ed/bikerental/PricingPolicy.java
@@ -4,6 +4,9 @@ import java.math.BigDecimal;
 import java.util.Collection;
 
 public interface PricingPolicy {
+    
     public void setDailyRentalPrice(BikeType bikeType, BigDecimal dailyPrice);
+    
     public BigDecimal calculatePrice(Collection<Bike> bikes, DateRange duration);
+    
 }
