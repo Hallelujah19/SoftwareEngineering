e661d7d40fef7548e8163a50b5a4a036f0c9e19d
hoffmannMuki <47600141+hoffmannMuki@users.noreply.github.com>
2019-11-23 22:44:55 +0000

    Update ImplementPricingPolicyTest.java

--- src/uk/ac/ed/bikerental/ImplementPricingPolicyTest.java
+++ src/uk/ac/ed/bikerental/ImplementPricingPolicyTest.java
@@ -30,6 +30,39 @@ class ImplementPricingTest {
 	@Test
 	void calculatePriceTest() {
 	
+	BigDecimal test_rate = BigDecimal.valueOf(0.05);
+ 	BigDecimal test_raw_price = BigDecimal.valueOf(0);
+ 	TestData.createBikeTypes(20);
+	for (BikeType bikeType : TestData.typeList) {
+		testProvider.addNewBikeType(bikeType.getTypeName(),1,BikeType.getReplacementValue());
+		}
+ 	
+	TestData.createBikes(20);
+	testBikes.addAll(TestData.bikeList);
+	for (Bike bike: TestData.bikeList) {
+		testProvider.setBike(bike);
+		}
+ 	
+	testBikes.addAll(TestData.bikeList);
+ 	TestData.createDates(20); // duration is 5 days so rate is 0.05
+	
+	TestData.createPrices(20);
+	 for(int i=0;i< 20;i++){
+		 testProvider.setDailyPricePerBikeType(TestData.typeList.get(i),TestData.priceList.get(i));
+		 }
+	
+ 
+ 	for (Bike bike : testProvider.getBikes()) {
+		
+	test_raw_price = test_raw_price.add(testProvider.getDailyPricePerBikeType(bike.getBikeType()).multiply(BigDecimal.valueOf(5)));
+	
+	} 
+	
+	Result = test_raw_price.multiply(test_rate);
+	
+        AssertEquals(result,ImplementPricing.calculatePrice(TestData.bikeList,TestData.dateRanges.get(0)));
+        
+		
 		
 		
 		
