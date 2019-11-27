6a3dcf5e7daaa6a71ad0ff2606d04d12bcdba092
Hallelujah19 <43917528+Hallelujah19@users.noreply.github.com>
2019-11-27 15:36:24 +0000

    Update TestData.java

--- src/uk/ac/ed/bikerental/TestData.java
+++ src/uk/ac/ed/bikerental/TestData.java
@@ -1,119 +1,120 @@
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
-import org.junit.jupiter.api.Test;
-
-class ImplementPricingTest {
-
-	Location testLocation = new Location("testPostCode", "testAddress");
-	OpeningHours testHours = new OpeningHours();
-	BikeProvider testProvider = new BikeProvider("providerName", testLocation, testHours);
-
-	ImplementPricing testImplementor = new ImplementPricing(testProvider);
-
-	BigDecimal value = BigDecimal.valueOf(200);
-	BikeType testBikeType = new BikeType("name", value);
-	BigDecimal testPrice = BigDecimal.valueOf(100);
-
+public class TestData {
 	
-	@Test
-	void SetDailyRentalPriceTest() {
+ static BigDecimal one = BigDecimal.valueOf(1);
+ static ArrayList<BikeType> typeList = new  ArrayList<BikeType>();
+ static ArrayList<Bike> bikeList = new  ArrayList<Bike>();
+ static ArrayList<BigDecimal> priceList = new ArrayList<BigDecimal>();
+ static ArrayList<DateRange> dateRanges = new ArrayList<>();
+ 
+ static ArrayList<Bike> bikeList2 = new  ArrayList<Bike>();
+ static ArrayList<Bike> bikeList5 = new  ArrayList<Bike>();
+ static ArrayList<Bike> bikeList8 = new  ArrayList<Bike>();
+ static ArrayList<Bike> bikeList15 = new  ArrayList<Bike>();
 
-		testImplementor.setDailyRentalPrice(testBikeType, testPrice);
+ 
+ static void createBikesTypes(int number) {
+	 BigDecimal value = BigDecimal.valueOf(100);
+	  StringBuilder string = new StringBuilder();
+	 
+	 for(int i=0;i<number;i++) {
+		string.append("type"+i);
+		value = value.add(one);
+		BikeType bikeType= new BikeType(string.toString(),value);
+		typeList.add(bikeType);
+		string.delete(0,("type"+i).length());
 
-		System.out.println(testProvider.getDailyPricePerBikeType(testBikeType));
+	 }
+	 
+ }
+ 
+ 
+ static void createPrices(int number) {
+	 
+	 BigDecimal basePrice =BigDecimal.valueOf(1000);
 
-		assertEquals(testPrice.stripTrailingZeros(),
-				testProvider.getDailyPricePerBikeType(testBikeType).stripTrailingZeros());
+	 for(int i=0;i<number;i++) {
+			basePrice = basePrice.add(one);
+			priceList.add(basePrice);
+		 }
+ }
+ 
+ 
+ static void createBikes(int number) {
+	 BigDecimal value = BigDecimal.valueOf(500);
+	 StringBuilder string = new StringBuilder();
+	 
+	 for(int i=0;i<number;i++) {
+		string.append("ID"+i);
+		value = value.add(one);
+		Bike bike= new Bike(string.toString(),value,typeList.get(i));
+		bikeList.add(bike);
+		string.delete(0,("ID"+i).length());
+	 }
+	 
+ }
+ 
+ 
+ static void createDates(int number) {
+	 
+	 for(int i=0;i < number; i++) {
+		 LocalDate start = LocalDate.of(2019,6,i+1);
+		 LocalDate end = null;
+		 
+		 if (i % 4 == 0) {
+		 end = start.plusDays(1);
+		 DateRange dateRange = new DateRange(start,end);
+		 dateRanges.add(dateRange);
+		 bikeList.get(i).setDateRange(dateRange);
+		 bikeList2.add(bikeList.get(i));
+		 } else if (i % 4 == 1) {
+		 end = start.plusDays(4);
+		 DateRange dateRange = new DateRange(start,end);
+		 dateRanges.add(dateRange);
+		 bikeList.get(i).setDateRange(dateRange);
+		 bikeList5.add(bikeList.get(i));
+		 } else if (i % 4 == 2) {
+		 end = start.plusDays(7);
+		 DateRange dateRange = new DateRange(start,end);
+		 dateRanges.add(dateRange);
+		 bikeList.get(i).setDateRange(dateRange);
+		 bikeList8.add(bikeList.get(i));
+		 } else if (i % 4 == 3) {
+		 end = start.plusDays(14);
+		 DateRange dateRange = new DateRange(start,end);
+		 dateRanges.add(dateRange);
+		 bikeList.get(i).setDateRange(dateRange);
+		 bikeList15.add(bikeList.get(i));
 
-	}
-
-
+		 }
+		
+	 }
+	 
+	 
+	 
+	 
+	 
+ }
+ 
+ 
 	
-	@Test
-	void calculatePriceTest() {
-
-		BigDecimal five = BigDecimal.valueOf(5);
-		BigDecimal eight = BigDecimal.valueOf(8);
-		BigDecimal fifteen = BigDecimal.valueOf(15);
-		BigDecimal two = BigDecimal.valueOf(2);
-
-		BigDecimal decrement = BigDecimal.valueOf(1-0.05);
-		BigDecimal decrement1 = BigDecimal.valueOf(1-0.1);
-		BigDecimal decrement2 = BigDecimal.valueOf(1-0.15);
-		BigDecimal decrement3 = BigDecimal.valueOf(1);
-		
-		BigDecimal test_raw_price = BigDecimal.valueOf(0);
-		BigDecimal test_raw_price1 = BigDecimal.valueOf(0);
-		BigDecimal test_raw_price2 = BigDecimal.valueOf(0);
-		BigDecimal test_raw_price3 = BigDecimal.valueOf(0);
-
-		
-		TestData.createBikesTypes(20);
-
-		for (BikeType bikeType : TestData.typeList) {
-			testProvider.addNewBikeType(bikeType.getTypeName(), 1, bikeType.getReplacementValue());
-		}
-
-		TestData.createBikes(20);
-
-		for (Bike bike : TestData.bikeList) {
-			testProvider.setBike(bike);
-		}
-         
-		TestData.createDates(20); 
-
-		TestData.createPrices(20);
-		
-		for (int i = 0; i < 20; i++) {
-			testProvider.setDailyPricePerBikeType(TestData.typeList.get(i), TestData.priceList.get(i));
-		}
-
-		//setting the ranges of dateRanges for bikes depending on the indices
-		
-		for (int i=0 ;i<20;i++) {
-			testProvider.getBikes().get(i).setDateRange(TestData.dateRanges.get(i));
-					}
-		
-		
-		//calculating raw prices after grouping the bikes with the same dateRanges and calculating the rate for each group
-		
-		for (Bike bike : testProvider.getBikes()) {
-			
-			BigDecimal days = BigDecimal.valueOf(bike.getDateRange().getDuration());
-			
-			if(days.equals(two)) {
-			test_raw_price3 = test_raw_price3.add(testProvider.getDailyPricePerBikeType(bike.getBikeType()).multiply(days));
-			
-			}else if(days.equals(five)) {
-				test_raw_price = test_raw_price.add(testProvider.getDailyPricePerBikeType(bike.getBikeType()).multiply(days));
+/* static void createShopLocations(int number) {
+	 
+	 for (int i = 0; i < number; i++) {
+	 	String 
+		 
+	 }
+	 
+ }
 	
-				
-			}else if(days.equals(eight)) {
-				test_raw_price1 = test_raw_price1.add(testProvider.getDailyPricePerBikeType(bike.getBikeType()).multiply(days));
 
-			}else if(days.equals(fifteen)) {
-				test_raw_price2 = test_raw_price2.add(testProvider.getDailyPricePerBikeType(bike.getBikeType()).multiply(days));
-
-			}
-			
-			
-		}
-		
-		
-		BigDecimal result = test_raw_price.multiply(decrement);
-		BigDecimal result1 = test_raw_price1.multiply(decrement1);
-		BigDecimal result2 = test_raw_price2.multiply(decrement2);
-		BigDecimal result3 = test_raw_price3.multiply(decrement3);
-		
-		
-		assertEquals(result, testImplementor.calculatePrice(TestData.bikeList5, TestData.dateRanges.get(1)));
-		assertEquals(result1, testImplementor.calculatePrice(TestData.bikeList8, TestData.dateRanges.get(2)));
-		assertEquals(result2, testImplementor.calculatePrice(TestData.bikeList15, TestData.dateRanges.get(3)));
-		assertEquals(result3, testImplementor.calculatePrice(TestData.bikeList2, TestData.dateRanges.get(0)));
-
-	//	System.out.println(test_raw_price);
-	}
-
+ static void createProviders(int number) {
+	 
+	 for (int j = 0; j < number; j++0 {
+	 	
+	 }*/
+ 
 }
