1b966557ab38987d6eaac89c7da1a5744429b6c0
Hallelujah19 <43917528+Hallelujah19@users.noreply.github.com>
2019-11-28 02:17:40 +0000

    Update Booking.java

--- src/uk/ac/ed/bikerental/Booking.java
+++ src/uk/ac/ed/bikerental/Booking.java
@@ -14,34 +14,32 @@ public class Booking implements Deliverable {
 	private Quote quote; // set from customer attribute
 	private Customer customer;
 
-	public Booking(Customer customer, String bookingNumber) {
+
+//change the constructor as the unique number is set from within the system
+public Booking(Customer customer) {
 		this.customer = customer;
 		this.quote = customer.getChosenQuote();
 		this.setBookingNumber(bookingNumber); // immediately set unique booking number
 	}
 
-	public void bookQuote() { // execute this method if the customer 
+
+public void bookQuote() { // execute this method if the customer 
 							  // has decided to book a quote
 		assert isBooked == true;
-		for (int i = 0; i < QuoteFinder.getAllProviders().size(); i++) {
-			// pick out the bike provider
-			if (QuoteFinder.getAllProviders().get(i).equals(quote.getBikeProvider())) { 
-				for (String bikeId : quote.getBikeIds()) {
-				   for (Bike bike : QuoteFinder.getAllProviders().get(i).getBikes()) {
-						if (bike.getBikeId().equals(bikeId)) { // find bike whose id matches what we have
-							// reserve upon booking
-							QuoteFinder.getAllProviders().get(i).updateBikeStatus(bikeId, BikeStatus.RESERVED);
-							break; // go to next bikeId
-						}
-					}
-				}
-			}
-			else
-				continue;
-		}
-		// proceed to schedule delivery
+		
+			statusSetter(BookingStatus.BOOKED.toString(),BikeStatus.RESERVED.toString());
+			
+		   // proceed to schedule delivery
 		if (customer.getMode().equals(CollectionMode.DELIVERY)) {
-			;
+			
+			//DeliveryServiceFactory;
+			DeliveryService deliveryService = DeliveryServiceFactory.getDeliveryService();
+			
+		    deliveryService.scheduleDelivery(this, quote.getBikeProvider().getShopLocation(),
+		    										customer.getLocation(),customer.getDateRange().getStart());
+			
+			
+			
 		}
 	}
 
@@ -62,7 +60,8 @@ public class Booking implements Deliverable {
 	}
 
 	public void setBookingNumber(String bookingNumber) {
-		this.bookingNumber = bookingNumber;
+		
+		this.bookingNumber = "BN"+QuoteFinder.NumberOfRequests;
 	}
 	
 	public boolean getIsBooked() {
@@ -73,13 +72,55 @@ public class Booking implements Deliverable {
 		this.isBooked = isBooked;
 	}
 	
-	public void onPickup() {
-		// updates booking and bike status on pick up
-		BigInteger big;
+	public void statusSetter(String bkingStatus,String bkStatus) {
+		
+		BikeStatus    bikeStatus = BikeStatus.valueOf(bkStatus);
+		BookingStatus bookingStatus = BookingStatus.valueOf(bkingStatus);
+		
+		for(int i = 0;i<QuoteFinder.getAllProviders().size();i++) {
+			
+			if (QuoteFinder.getAllProviders().get(i).equals(quote.getBikeProvider())) { 
+				if (QuoteFinder.getAllProviders().get(i).equals(quote.getBikeProvider())) { 
+					for (String bikeId : quote.getBikeIds()) {
+					   for (Bike bike : QuoteFinder.getAllProviders().get(i).getBikes()) {
+							if (bike.getBikeId().equals(bikeId)) { // find bike whose id matches what we have
+								// reserve upon booking
+								QuoteFinder.getAllProviders().get(i).updateBikeStatus(bikeId, bikeStatus);
+								this.setStatus(bookingStatus);
+								break; // go to next bikeId
+							}
+						}
+					}
+				}
+				else
+					continue;
+					}
+
+			}
+		
 		
 	}
 	
+	
+	
+	
+	
+	public void onPickup() {
+		// updates bike status on pick up
+		//bike status changes to Dispatched
+		statusSetter(BookingStatus.DISPATCHED.toString(),BikeStatus.PICKED_UP.toString());
+		
+	}
+	
+
 	public void onDropoff() {
+		//bike status changes to Hired
+		//booking status  Delivered
+		
+		statusSetter(BookingStatus.DELIVERED.toString(),BikeStatus.HIRED.toString());
+
+		
+		
 		
 	}
 
